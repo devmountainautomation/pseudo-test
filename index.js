605288bb6a4f0e871728dc91d3513e2410dbf4e5
rnoelle <reidnoelle2@gmail.com>
2016-09-01 20:23:43 -0600

    sent message through PubNub

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const express = require('express')
     , bodyParser = require('body-parser')
     , config = require('./config')
-    , stripe = require('stripe')(config.testSecretKey);
+const PubNub = require ('pubnub');
+
+// // get/create/store UUID
+// var UUID = PubNub.db.get('session') || (function(){
+//     var uuid = PubNub.uuid();
+//     PubNub.db.set('session', uuid);
+//     return uuid;
+// })();
+
+var pubnub = new PubNub({
+    subscribeKey: config.testSubscribeKey,
+    publishKey: config.testPublishKey,
+    secretKey: config.testSecretKey,
+    logVerbosity: true,
+    uuid: "0bbb304b-13ae-4fd7-9f5e-e3596e520b6c",
+    ssl: true,
+    presenceTimeout: 130,
+})
+
 
 const app = express();
 app.use(bodyParser.json());
@@ -9,26 +27,28 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/node_modules', express.static('./node_modules'));
 app.use(express.static('./public'));
 
-app.post('/api/charge', function(req, res) {
-  // Get the credit card details submitted by the form
-  var token = req.body.stripeToken; // Using Express
-  // Create a charge: this will charge the user's card
-  var charge = stripe.charges.create({
-   amount: req.body.price, // Amount in cents
-   currency: "usd",
-   source: token,
-   description: "Example charge"
-  }, function(err, charge) {
-   if (err && err.type === 'StripeCardError') {
-     // The card has been declined
-   } else {
-     // Store order status
-     res.status(200).send(charge);
-   }
-  });
+pubnub.addListener({
+  message: function(message) {
+    console.log(message);
+  },
+  presence: function(presence) {
+    console.log(presence)
+  },
+  status: function(status) {
+    console.log(status);
+  }
 })
 
-const port = 3000;
+pubnub.subscribe({
+    channels: ['my_channel'],
+    withPresence: true // also subscribe to presence instances.
+})
+
+app.post('/test', function (req, res, next) {
+  console.log(req.body);
+})
+
+const port = 5000;
 app.listen(port, function() {
   console.log('Listening on port');
 })
